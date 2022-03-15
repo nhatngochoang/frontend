dbdb6c18b91749ea954ec4a7ade26b3e88453cbe
tahn-0102 <ongthoxunu@gmail.com>
2022-03-16 01:10:44 +0700

    Redux Saga: 04 - Add Redux Saga vào Redux Toolkit 🎉

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # Init 
 yarn create react-app my-app --template redux-typescript
-
+yarn add redux-saga
     
 # add  "baseUrl": "./src" in tsconfig
 
@@ -8,3 +8,8 @@ yarn create react-app my-app --template redux-typescript
 
 # useDispatch ➤ useAppDispatch
 # useSelector ➤ useAppSelector
+
+yield takeEvery('*', log)
+yield takeEvery('counter/increment', log)
+yield takeEvery(increment().type, log)  {increment} ➤ export from counterSlice.actions
+
--- src/app/store.ts
+++ src/app/store.ts
@@ -1,10 +1,17 @@
 import { configureStore, ThunkAction, Action } from '@reduxjs/toolkit';
 import counterReducer from '../features/counter/counterSlice';
+import createSagaMiddleware from 'redux-saga';
+
+const sagaMiddleware = createSagaMiddleware(); // Step 1
 
 export const store = configureStore({
   reducer: {
     counter: counterReducer,
   },
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      thunk: true,
+    }).concat(sagaMiddleware), // Step 1
 });
 
 export type AppDispatch = typeof store.dispatch;
