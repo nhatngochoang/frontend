d1cf372de9eeefa768966df99fbfe786179f0566
tahn-0102 <ongthoxunu@gmail.com>
2022-03-21 19:14:33 +0700

    Redux Saga: 21 - Admin Layout với CSS Grid and Material UI

--- src/features/auth/pages/LoginPage.tsx
+++ src/features/auth/pages/LoginPage.tsx
@@ -4,52 +4,46 @@ import * as React from 'react';
 import { authActions } from '../authSlice';
 
 const useStyles = makeStyles((theme) => ({
-  root: {
-    display: 'flex',
-    flexFlow: 'row nowrap',
-    justifyContent: 'center',
-    alignItems: 'center',
-    minHeight: '100vh',
-  },
-  box: {
-    padding: theme.spacing(3), // 1 unit 8px
-  },
+   root: {
+      display: 'flex',
+      flexFlow: 'row nowrap',
+      justifyContent: 'center',
+      alignItems: 'center',
+      minHeight: '100vh',
+   },
+   box: {
+      padding: theme.spacing(3), // 1 unit 8px
+   },
 }));
 
 export function LoginPage() {
-  const classes = useStyles();
-  const dispatch = useAppDispatch();
+   const classes = useStyles();
+   const dispatch = useAppDispatch();
 
-  const isLogging = useAppSelector((state) => state.auth.logging);
+   const isLogging = useAppSelector((state) => state.auth.logging);
 
-  const handleLoginClick = () => {
-    // TODO: Get username + pwd from login form
-    dispatch(
-      authActions.login({
-        username: '',
-        password: '',
-      })
-    );
-  };
+   const handleLoginClick = () => {
+      // TODO: Get username + pwd from login form
+      dispatch(
+         authActions.login({
+            username: '',
+            password: '',
+         })
+      );
+   };
 
-  const handleLogoutClick = () => {
-    // TODO: Get username + pwd from login form
-    dispatch(authActions.logout());
-  };
-
-  return (
-    <div className={classes.root}>
-      <Paper elevation={1} className={classes.box}>
-        <Typography variant="h5" component="h1">
-          Student Management
-        </Typography>
-        <Box mt={4}>
-          <Button fullWidth variant="contained" color="primary" onClick={handleLoginClick}>
-            {isLogging && <CircularProgress size={20} color="secondary" />} &nbsp; Fake Login
-          </Button>
-          <button onClick={handleLogoutClick}>log out</button>
-        </Box>
-      </Paper>
-    </div>
-  );
+   return (
+      <div className={classes.root}>
+         <Paper elevation={1} className={classes.box}>
+            <Typography variant="h5" component="h1">
+               Student Management
+            </Typography>
+            <Box mt={4}>
+               <Button fullWidth variant="contained" color="primary" onClick={handleLoginClick}>
+                  {isLogging && <CircularProgress size={20} color="secondary" />} &nbsp; Fake Login
+               </Button>
+            </Box>
+         </Paper>
+      </div>
+   );
 }
