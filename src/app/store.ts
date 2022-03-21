a3a552208cd538e63037f6e7706e588840b561cc
tahn-0102 <ongthoxunu@gmail.com>
2022-03-21 22:58:06 +0700

    Redux Saga: 24 - Cài đặt slice cho trang dashboard

--- src/app/store.ts
+++ src/app/store.ts
@@ -3,23 +3,25 @@ import { connectRouter, routerMiddleware } from 'connected-react-router';
 import createSagaMiddleware from 'redux-saga';
 import authReducer from '../features/auth/authSlice';
 import counterReducer from '../features/counter/counterSlice';
+import dashboardReducer from '../features/dashboard/dashboardSlice';
 import { history } from '../utils';
 import rootSaga from './rootSaga';
 
 const rootReducer = combineReducers({
-  router: connectRouter(history), // connected-react-router
-  counter: counterReducer,
-  auth: authReducer,
+   router: connectRouter(history), // connected-react-router
+   counter: counterReducer,
+   auth: authReducer,
+   dashboard: dashboardReducer,
 });
 
 const sagaMiddleware = createSagaMiddleware(); // Step 1 Saga
 
 export const store = configureStore({
-  reducer: rootReducer,
-  middleware: (getDefaultMiddleware) =>
-    getDefaultMiddleware({
-      thunk: true,
-    }).concat(sagaMiddleware, routerMiddleware(history)), // Step 1 Saga + connected-react-router ➤ for dispatching history actions
+   reducer: rootReducer,
+   middleware: (getDefaultMiddleware) =>
+      getDefaultMiddleware({
+         thunk: true,
+      }).concat(sagaMiddleware, routerMiddleware(history)), // Step 1 Saga + connected-react-router ➤ for dispatching history actions
 });
 
 sagaMiddleware.run(rootSaga); // Step 1 Saga
@@ -27,8 +29,8 @@ sagaMiddleware.run(rootSaga); // Step 1 Saga
 export type AppDispatch = typeof store.dispatch;
 export type RootState = ReturnType<typeof store.getState>;
 export type AppThunk<ReturnType = void> = ThunkAction<
-  ReturnType,
-  RootState,
-  unknown,
-  Action<string>
+   ReturnType,
+   RootState,
+   unknown,
+   Action<string>
 >;
