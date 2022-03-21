6e9dd1bea30ea4a5bd1627ff4d8eb4b821d799fd
tahn-0102 <ongthoxunu@gmail.com>
2022-03-22 00:25:05 +0700

    Redux Saga: 25 - Hack não với saga của dashboard 😜

--- src/app/rootSaga.ts
+++ src/app/rootSaga.ts
@@ -2,7 +2,8 @@ import counterSaga from '../features/counter/counterSaga';
 import { all } from 'redux-saga/effects';
 import { incrementAsync } from '../sagas';
 import authSaga from '../features/auth/authSaga';
+import dashboardSaga from '../features/dashboard/dashboardSaga';
 
 export default function* rootSaga() {
-  yield all([counterSaga(), incrementAsync(), authSaga()]);
+   yield all([counterSaga(), incrementAsync(), authSaga(), dashboardSaga()]);
 }
--- src/features/dashboard/index.tsx
+++ src/features/dashboard/index.tsx
@@ -1,7 +1,35 @@
 import * as React from 'react';
+import { useAppDispatch, useAppSelector } from '../../app/hooks';
+import {
+   dashboardActions,
+   selectDashboardLoading,
+   selectDashboardStatistics,
+   selectHighestStudentList,
+   selectLowestStudentList,
+   selectRankingByCityList,
+} from './dashboardSlice';
 
 export interface DashboardProps {}
 
 export default function Dashboard(props: DashboardProps) {
+   const dispatch = useAppDispatch();
+   const loading = useAppSelector(selectDashboardLoading);
+   const statistics = useAppSelector(selectDashboardStatistics);
+   const highestStudentList = useAppSelector(selectHighestStudentList);
+   const lowestStudentList = useAppSelector(selectLowestStudentList);
+   const rankingByCityList = useAppSelector(selectRankingByCityList);
+
+   console.log({
+      loading,
+      statistics,
+      highestStudentList,
+      lowestStudentList,
+      rankingByCityList,
+   });
+
+   React.useEffect(() => {
+      dispatch(dashboardActions.fetchData());
+   }, [dispatch]);
+
    return <div>Dashboard</div>;
 }
