1f6c0d60eef679f2d8be0acd0ad6450a2ac88a26
tahn-0102 <ongthoxunu@gmail.com>
2022-03-24 00:48:45 +0700

    Update Demo.js

--- src/react-hook-form-v7/Demo.js
+++ src/react-hook-form-v7/Demo.js
@@ -1,20 +1,33 @@
 import { useForm } from "react-hook-form"
 
+// https://blog.logrocket.com/react-hook-form-complete-guide/
 export default function Demo() {
-   const { register, handleSubmit, formState: { errors } } = useForm()
+   const { register, handleSubmit, formState: { errors } } = useForm({
+      mode: "onBlur"
+      // mode: "onChange" is default
+   })
 
+   const validateOptions = {
+      // defined validation here
+      password: { required: true, minLength: 6 }
+
+   }
    const onSubmit = (formData) => {
       console.log(formData)
    }
 
+   const onErrors = (errors) => {
+      console.log(errors);
+   }
+
    // after validate success, call onSubmit()
    return (
-      <form onSubmit={handleSubmit(onSubmit)}>
+      <form onSubmit={handleSubmit(onSubmit, onErrors)}>
          <h1>React Hook Form</h1>
          <label htmlFor="name">Name</label>
          <input type="text" name="name" id="name" placeholder="Name" {...register("name", { required: true })} />
          < label htmlFor="password">Password</label>
-         <input type="password" name="password" id="password" placeholder="Password" {...register("password", { required: true, minLength: 6 })} />
+         <input type="password" name="password" id="password" placeholder="Password" {...register("password", validateOptions.password)} />
          <label htmlFor="email">Email</label>
          <input type="email" name="email" id="email" placeholder="Email" {...register("email", { required: true, pattern: /[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,4}$/i })} />
          {/* Handle Errors */}
