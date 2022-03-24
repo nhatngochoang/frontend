f91d74a0cdc4a8f7212a2b295d9a5aed1159709e
tahn-0102 <ongthoxunu@gmail.com>
2022-03-24 19:04:21 +0700

    Redux Saga: 44 - Làm form validation đơn giản với Yup

--- src/features/student/components/StudentForm.tsx
+++ src/features/student/components/StudentForm.tsx
@@ -5,7 +5,39 @@ import React from 'react';
 import { useForm } from 'react-hook-form';
 import { useAppSelector } from '../../../app/hooks';
 import { selectCityOptions } from '../../city/citySlice';
+import { yupResolver } from '@hookform/resolvers/yup';
+import * as yup from 'yup';
 
+const schema = yup.object().shape({
+   name: yup
+      .string()
+      .required('Please enter name.')
+      .test('two-words', 'Please enter at least two words', (value) => {
+         if (!value) return true;
+
+         const parts = value?.split(' ') || [];
+         return parts.filter((x) => Boolean(x)).length >= 2;
+      }),
+   age: yup
+      .number()
+      .positive('Please enter a positive number.')
+      .min(18, 'Min is 18')
+      .max(60, 'Max is 60')
+      .integer('Please enter an integer.')
+      .required('Please enter age.')
+      .typeError('Please enter a valid number.'),
+   mark: yup
+      .number()
+      .min(0, 'Min is 0')
+      .max(10, 'Max is 10')
+      .required('Please enter mark.')
+      .typeError('Please enter a valid number.'),
+   gender: yup
+      .string()
+      .oneOf(['male', 'female'], 'Please select either male or female.')
+      .required('Please select gender.'),
+   city: yup.string().required('Please select city.'),
+});
 export interface StudentFormProps {
    initialValues?: Student;
    onSubmit?: (formValues: Student) => void;
@@ -15,6 +47,7 @@ export default function StudentForm(props: StudentFormProps) {
    const { initialValues, onSubmit } = props;
    const { control, handleSubmit } = useForm<Student>({
       defaultValues: initialValues,
+      resolver: yupResolver(schema),
    });
    const cityOptions = useAppSelector(selectCityOptions);
 
