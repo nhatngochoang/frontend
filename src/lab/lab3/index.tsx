0799e415d14f688a9a8c515501ac65c501436196
tahn-0102 <ongthoxunu@gmail.com>
2022-03-28 15:47:51 +0700

    02 - form thêm mới lưu dữ liệu

--- src/lab/lab3/index.tsx
+++ src/lab/lab3/index.tsx
@@ -1,39 +1,136 @@
-import * as React from 'react';
+import { useState, useEffect } from 'react';
 import { StudentService } from './model';
 export interface Lab3Props {}
 
 export default function Lab3(props: Lab3Props) {
-   const students = new StudentService();
-   students.setData();
+   const [data, setData] = useState(() => {
+      const students = new StudentService();
+      students.setData();
+      return students.data;
+   });
+   const [code, setCode] = useState('');
+   const [name, setName] = useState('');
+   const [toan, setToan] = useState(0);
+   const [li, setLi] = useState(0);
+   const [hoa, setHoa] = useState(0);
+   const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+      e.preventDefault();
+      const newStudent = {
+         code,
+         name,
+         toan,
+         li,
+         hoa,
+      };
+      data.push(newStudent);
+      setData([...data]);
+   };
+   useEffect(() => {}, []);
    return (
       <div className="container">
-         <h2>Danh sách sinh viên</h2>
-         <table className="table">
-            <thead>
-               <tr>
-                  <th>Mã</th>
-                  <th>Họ tên</th>
-                  <th>Toán</th>
-                  <th>Lí</th>
-                  <th>Hóa</th>
-                  {/* <th>Điểm TB</th>
-                  <th>Xếp loại</th> */}
-               </tr>
-            </thead>
-            <tbody>
-               {students.data.map((s, sidx) => {
-                  return (
+         <div className="row">
+            <div className="col-md-4">
+               <form action="" method="post" onSubmit={handleSubmit}>
+                  <legend>Title</legend>
+                  <div className="form-group">
+                     <label htmlFor="code">Mã</label>
+                     <input
+                        type="text"
+                        className="form-control"
+                        placeholder="...mã"
+                        id="code"
+                        value={code}
+                        onChange={(e) => setCode(e.target.value)}
+                     />
+                  </div>
+                  <div className="form-group">
+                     <label htmlFor="name">Tên</label>
+                     <input
+                        type="text"
+                        className="form-control"
+                        placeholder="...tên"
+                        id="name"
+                        value={name}
+                        onChange={(e) => setName(e.target.value)}
+                     />
+                  </div>
+                  <div className="row">
+                     <div className="col-md-4">
+                        <div className="form-group">
+                           <label htmlFor="toan">Toán</label>
+                           <input
+                              type="number"
+                              className="form-control"
+                              placeholder="...toán"
+                              id="toan"
+                              value={toan}
+                              onChange={(e) => setToan(Number(e.target.value))}
+                           />
+                        </div>
+                     </div>
+                     <div className="col-md-4">
+                        <div className="form-group">
+                           <label htmlFor="li">Lí</label>
+                           <input
+                              type="number"
+                              className="form-control"
+                              placeholder="...lí"
+                              id="li"
+                              value={li}
+                              onChange={(e) => setLi(Number(e.target.value))}
+                           />
+                        </div>
+                     </div>
+                     <div className="col-md-4">
+                        <div className="form-group">
+                           <label htmlFor="hoa">Hóa</label>
+                           <input
+                              type="number"
+                              className="form-control"
+                              placeholder="...hóa"
+                              id="hoa"
+                              value={hoa}
+                              onChange={(e) => setHoa(Number(e.target.value))}
+                           />
+                        </div>
+                     </div>
+                  </div>
+
+                  <button type="submit" className="btn btn-primary">
+                     Thêm
+                  </button>
+               </form>
+            </div>
+            <div className="col-md-8">
+               <h2>Danh sách sinh viên</h2>
+               <table className="table">
+                  <thead>
                      <tr>
-                        <td>{s.code}</td>
-                        <td>{s.name}</td>
-                        <td>{s.toan}</td>
-                        <td>{s.li}</td>
-                        <td>{s.hoa}</td>
+                        <th>Mã</th>
+                        <th>Họ tên</th>
+                        <th>Toán</th>
+                        <th>Lí</th>
+                        <th>Hóa</th>
+                        {/* <th>Điểm TB</th>
+                  <th>Xếp loại</th> */}
                      </tr>
-                  );
-               })}
-            </tbody>
-         </table>
+                  </thead>
+                  <tbody>
+                     {data.map((s, sidx) => {
+                        return (
+                           <tr key={sidx}>
+                              <td>{s.code}</td>
+                              <td>{s.name}</td>
+                              <td>{s.toan}</td>
+                              <td>{s.li}</td>
+                              <td>{s.hoa}</td>
+                           </tr>
+                        );
+                     })}
+                  </tbody>
+               </table>
+            </div>
+         </div>
       </div>
    );
 }
