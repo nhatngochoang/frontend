8b9b751928e14295d797ca5fc81e99682867dd3d
TWINGER-NHATHN <stu715113037@hnue.edu.vn>
2022-08-22 17:19:50 +0700

    init project

--- src/app/store.ts
+++ src/app/store.ts
@@ -1,34 +1,10 @@
 import { Action, combineReducers, configureStore, ThunkAction } from '@reduxjs/toolkit';
-import { connectRouter, routerMiddleware } from 'connected-react-router';
-import createSagaMiddleware from 'redux-saga';
-import authReducer from '../features/auth/authSlice';
-import cityReducer from '../features/city/citySlice';
-import counterReducer from '../features/counter/counterSlice';
-import dashboardReducer from '../features/dashboard/dashboardSlice';
-import studentReducer from '../features/student/studentSlice';
-import { history } from '../utils';
-import rootSaga from './rootSaga';
 
-const rootReducer = combineReducers({
-   router: connectRouter(history), // connected-react-router
-   counter: counterReducer,
-   auth: authReducer,
-   dashboard: dashboardReducer,
-   student: studentReducer,
-   city: cityReducer,
-});
-
-const sagaMiddleware = createSagaMiddleware(); // Step 1 Saga
+const rootReducer = combineReducers({});
 
 export const store = configureStore({
    reducer: rootReducer,
-   middleware: (getDefaultMiddleware) =>
-      getDefaultMiddleware({
-         thunk: true,
-      }).concat(sagaMiddleware, routerMiddleware(history)), // Step 1 Saga + connected-react-router ➤ for dispatching history actions
 });
-
-sagaMiddleware.run(rootSaga); // Step 1 Saga
 
 export type AppDispatch = typeof store.dispatch;
 export type RootState = ReturnType<typeof store.getState>;
--- src/index.tsx
+++ src/index.tsx
@@ -1,38 +1,14 @@
-// import { BrowserRouter as Router } from 'react-router-dom';
-import { CssBaseline } from '@material-ui/core';
-import { ConnectedRouter } from 'connected-react-router';
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import App from './App';
 import { store } from './app/store';
 import './index.css';
-import { history } from './utils';
-import { ToastContainer } from 'react-toastify';
-import 'react-toastify/dist/ReactToastify.min.css';
-import CurrentUserContext from 'contexts/CurrentUserContext';
 
 ReactDOM.render(
    <React.Fragment>
       <Provider store={store}>
-         <ConnectedRouter history={history}>
-            {/* Reset CSS */}
-            <CssBaseline />
-            <CurrentUserContext>
-               <App />
-            </CurrentUserContext>
-         </ConnectedRouter>
-         <ToastContainer
-            position="top-right"
-            autoClose={5000}
-            hideProgressBar={false}
-            newestOnTop={false}
-            closeOnClick
-            rtl={false}
-            pauseOnFocusLoss
-            draggable
-            pauseOnHover
-         />
+         <App />
       </Provider>
    </React.Fragment>,
    document.getElementById('root')
